b4d7f4838fc50f1bdeb9e9f924e3d1385fc44acc
NickJoyce <actan-spb@mail.ru>
2022-06-01 17:32:25 +0300

    xx

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 #! python3
 # Выполняет поиск в google по запросу (query), открывает в браузере первые num_open страниц
 # выданных google в результатах поиска
-
 import webbrowser
 import requests
 from bs4 import BeautifulSoup
 
-query = 'Театры москвы'
-num_open = 5
 
-headers = {
-    "Accept":"*/*",
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
-}
+# Поисковый запрос в Google
+query = 'Театры москвы'
+# Количество ссылок полученных на поисковый запрос, по которым будет совершен переход и открыты соотвествующие вкладки
+num_open = 3
+
+headers = {"Accept": "*/*",
+           "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                         " Chrome/99.0.4844.84 Safari/537.36"}
 
 res = requests.get(f'https://www.google.com/search?q={query}&sourceid=chrome&ie=UTF-8', headers=headers)
 res.raise_for_status()
